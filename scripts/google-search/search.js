cce7d129cd9fd5a5f419ee038ae927b2cad12642
consciousm1n9 <rafael.cas.vil@gmail.com>
2023-05-25 12:11:02 -0600

    Update. Add Doc

--- scripts/google-search/search.js
+++ scripts/google-search/search.js
@@ -2,111 +2,124 @@ const puppeteer = require('puppeteer');
 const download = require('image-downloader');
 const fs = require("fs");
 
-//constants
-const search = 'corne keyboard';
-
+//constants, init folder
 const url = 'https://www.google.com.mx/imghp?hl=en&ogbl';
-const rootPath = `${__dirname}/result`;
-let folder = `${rootPath}/${new Date().getTime()}#${search.replace(" ", "-")}`;
-if(!fs.existsSync(rootPath))
-  fs.mkdirSync(rootPath);
-if (!fs.existsSync(folder)){
-  fs.mkdirSync(folder);
+const busqueda = 'corne keyboard';
+const rootDir = `${__dirname}/results/`;
+
+let file = `${rootDir}${new Date().getTime()}#${busqueda}`;
+if (!fs.existsSync(rootDir)){
+  fs.mkdirSync(rootDir);
+}
+if (!fs.existsSync(file)){
+  fs.mkdirSync(file);
 }
 
 (async () => {
-  //init
+//Init browser,tab(page) and navigate to url
   const browser = await puppeteer.launch({headless: false, defaultViewport: null});
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(10000);
-  await page.setDefaultTimeout(10000);
+  await page.setDefaultTimeout(5000); 
   await page.goto(url);
 
-  //Ignore charge of images to improvement performance
-    await page.setRequestInterception(true);
-    page.on('request', request => {
-      if (request.resourceType() === 'image')
-        request.abort();
-      else
-        request.continue();
-    });
+//Block image resources to improvement performance
+  await page.setRequestInterception(true);
+  page.on('request', request => {
+    if (request.resourceType() === 'image')
+      request.abort();
+    else
+      request.continue();
+  });
 
-  //search
-  await page.waitForSelector('input.gLFyf.gsfi');
-  await page.type('input[name=q]', search);
+//Search by criteria
+  await page.waitForSelector('textarea.gLFyf');
+  await page.type('textarea.gLFyf', busqueda);
   await page.keyboard.press('Enter');
   await page.waitForSelector('div.bRMDJf.islir img');
-  await delay(3);
 
+  let flag = true;
   let contador = 0;
-  let attempt = 0;
-  let cont = true;
-  
-  while(cont){
-    try{
-      let elem = await page.$(`[data-ri="${contador}"]`);
-      if (elem != undefined){
-        attempt = 0;
-        const resource = await elem.$("img");
-        let img = await(await resource.getProperty('src')).jsonValue();
-        if (img == ""){
-          img = await page.evaluate(el => el.getAttribute('data-src'), resource);
-        }
-        prepareImage(img, contador);
-        contador++;
-      }else{
-        console.log('elem undefined -> scroll to bottom');
-        attempt++;
-        const previousHeight = await page.evaluate('document.body.scrollHeight');
-        await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-        try{
-          await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-        }catch(err){
-          console.log('Error 2:'+err);
-          if (attempt == 3){
-            console.log('Third attempt. Final search');
-            await browser.close();
-          }
-          let next = await page.$$("input.mye4qd");
-          if ( next == undefined ){
-            console.log('Final search');
-            await browser.close();
-          }else{
-            console.log('click on bottom button.');
-            await page.click('input.mye4qd');
-          }
-        }
+//Get all div with image loaded
+  let results = await page.$$('div.isv-r.PNCib.MSM1fd.BUooTd');
+
+  while(flag){
+    for (let i = contador; i < results.length; i++) {
+/**
+ * Get img element and src property of each one.
+ * Get dom elemnt id and download resource.
+ */
+      const obj = await results[i].$("img");
+      let img = await(await obj.getProperty('src')).jsonValue();
+      if(img==''){
+        img = await page.evaluate(el => el.getAttribute('data-src'), obj);
       }
-    }catch(e){
-      console.log('Error 1:'+e+" Exit");
-      cont = false;
+      const nameid = await page.evaluate(el => el.getAttribute('data-ri'), results[i]);
+      prepareImage(img, nameid);
+    }
+
+//scroll to bottom to loading more results
+    console.log('scroll to bottom');
+    const previousHeight = await page.evaluate('document.body.scrollHeight');
+    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+
+/**
+ * Wait for scroll height > previousheight.
+ * If trigger timeout try to click on button 'More results',
+ * if not exists finish execution.
+ */
+    try{
+      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+    }catch(err){
+      console.log('No more scroll down.');
+      try{
+        const next = await page.$("input.mye4qd");
+        await next.click();
+        console.log('Click on "More results" button');
+      }catch(e){
+        console.log('Final search');
+        flag = false;
+      }
+    }
+/**
+ * Obtain again all div with images and search for the last item we worked on.
+ * Set counter on next.
+ */
+    let results2 = await page.$$('div.isv-r.PNCib.MSM1fd.BUooTd');
+    console.log('New length:'+results2.length);
+    let fitem1 = await page.evaluate(el => el.getAttribute('data-ri'),results[results.length-1]);
+
+    for (let index = results2.length-1; index > 0; index--) {
+      fitem2 = await page.evaluate(el => el.getAttribute('data-ri'),results2[index]);
+      if( fitem1 == fitem2){
+        contador = index+1;
+        results = results2;
+        break;
+      }
     }
   }
   await browser.close();
 })();
-
+/**
+ * Get image. If base64 image or http resource. 
+ * @param {string} imgSrc - Source image 
+ * @param {string} imgcontador - Counter of dom element
+ */
 async function prepareImage(imgSrc, imgcontador){
   if (imgSrc.includes("http")){
-    console.log('imgSrc:'+imgSrc+'|cont:'+imgcontador);
-    saveImageToDisk(imgSrc, folder+"/"+imgcontador+".jpg");
+    console.log("i:"+imgcontador+": "+imgSrc);
+    saveImageToDisk(imgSrc, file+"/"+imgcontador+".jpg")
   }else{
-    console.log('base64:...|cont:'+imgcontador);
+    console.log("i:"+imgcontador+": "+imgSrc.substring(0, 30));
     let buf = imgSrc.split(';base64,').pop();
-    fs.writeFileSync(folder+"/"+imgcontador+".jpg", buf, {encoding: 'base64'}, function(err){
+    fs.writeFileSync(file+"/"+imgcontador+".jpg", buf, {encoding: 'base64'}, function(err){
       console.log('err:'+err);
     });
   }
 }
 
-function delay(n){
-    return new Promise(function(resolve){
-        setTimeout(resolve,n*1000);
-    });
-}
 function saveImageToDisk(url, localPath) {
     return download.image({
       url,
       dest: localPath 
-    }).then(({filename}) => {
-    }).catch((err) => console.log(err));
+    }); 
 }
